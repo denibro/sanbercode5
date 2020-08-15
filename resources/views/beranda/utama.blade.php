d73ff42682cb3317ab80dfc66e2c95ef16f805ae
riswanibrahim93 <ibrahim93vikings@gmail.com>
2020-08-15 11:37:21 +0700

    membuat link profile

--- resources/views/beranda/utama.blade.php
+++ resources/views/beranda/utama.blade.php
@@ -14,28 +14,7 @@
     {{ session('status') }}
 </div>
 @endif
-<<<<<<< HEAD
-<div class="text-center font-weight-bold pt-3">
-  <h3>Beranda</h3>
-</div>
-<ul class="list-group">
-    @foreach($pertanyaans as $pertanyaan)
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>
-            <h4 class="font-weight-bold">{{ $pertanyaan->judul }}</h5>
-                <h5>{{ $pertanyaan->isi }}</h6>
-                    <h6 class="text-muted">{{ $pertanyaan->created_at }}</h6>
-                    <h6 class="text-muted">{{ $pertanyaan->updated_at }}</h6>
-        </span>
-        <a href="{{route('jawaban.create')}}" class="badge badge-primary badge-pill">Bantu Jawab</a>
-        <a href="" class="badge badge-primary badge-pill">Upvote</a>
-        <a href="" class="badge badge-primary badge-pill">Downvote</a>
-    </li>
-    @endforeach
-</ul>
-=======
 
-<div class="container">
     <ul class="list-group mt-3">
         @foreach($pertanyaans as $pertanyaan)
         <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -58,7 +37,5 @@
         </li>
         @endforeach
     </ul>
-</div>
->>>>>>> 30bfda3f3ab1891f2ccc0167ecc1b523fd4d8b58
-
+    
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pertanyaan','PertanyaanController@index');
-Route::get('/pertanyaan/create','PertanyaanController@create');
-Route::post('/pertanyaan','PertanyaanController@store');
-Route::get('/pertanyaan/{pertanyaan_id}','PertanyaanController@show');
-Route::get('/pertanyaan/{pertanyaan_id}/edit','PertanyaanController@edit');
-Route::put('/pertanyaan/{pertanyaan_id}','PertanyaanController@update');
-Route::delete('/pertanyaan/{pertanyaan_id}','PertanyaanController@destroy');
-=======
-Route::get('/', 'PertanyaanController@index');
+// Route::get('/pertanyaan','PertanyaanController@index');
+// Route::get('/pertanyaan/create','PertanyaanController@create');
+// Route::post('/pertanyaan','PertanyaanController@store');
+// Route::get('/pertanyaan/{pertanyaan_id}','PertanyaanController@show');
+// Route::get('/pertanyaan/{pertanyaan_id}/edit','PertanyaanController@edit');
+// Route::put('/pertanyaan/{pertanyaan_id}','PertanyaanController@update');
+// Route::delete('/pertanyaan/{pertanyaan_id}','PertanyaanController@destroy');
+// =======
+// Route::get('/', 'PertanyaanController@index');
 
-Route::get('/pertanyaan/create', 'PertanyaanController@create');
->>>>>>> 30bfda3f3ab1891f2ccc0167ecc1b523fd4d8b58
+// Route::get('/pertanyaan/create', 'PertanyaanController@create');
+// >>>>>>> 30bfda3f3ab1891f2ccc0167ecc1b523fd4d8b58
 Route::get('/beranda', 'PertanyaanController@index');
 Route::resource('jawaban', 'JawabanController');
 Route::get('/profile', 'PertanyaanController@index2')->name('pertanyaan.profile');
